027ac982764c5e7092c207cdd0d2cb148c6f7311
Bhushan <bhushan@Bhushans-Mac.local>
2021-06-08 05:57:00 -0700

    Implemented calculation of total wage for a month

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -3,19 +3,30 @@ let isFullTime = 1
 let isPartTime = 2
 let wagePerHour = 20
 var employeeHours = 0
+var totalEmployeeWage = 0
 
-var employeeAttendance = Int.random(in: 0...2)
-print(employeeAttendance)
+print("<-----Employee Wage Calculator----->")
 
-if (employeeAttendance == isAbsent) {
-    print("Employee is Absent")
-} else if (employeeAttendance == isFullTime) {
-    print("Employee is Working Full Time")
-    employeeHours = 8
-} else {
-    print("Employee is Woring Part Time")
-    employeeHours = 4
+for i: Int in 1...22 {
+    let employeeAttendance = Int.random(in: 0...2)
+    
+    if (employeeAttendance == isAbsent) {
+        print("Employee is Absent on Day", i)
+        employeeHours = 0
+        
+    } else if (employeeAttendance == isFullTime) {
+        print("Employee is Working Full Time on Day", i)
+        employeeHours = 8
+        
+    } else {
+        print("Employee is Woring Part Time on Day", i)
+        employeeHours = 4
+        
+    }
+    let employeeWage = employeeHours * wagePerHour
+    print("Wage for day", i, "=", employeeWage)
+    totalEmployeeWage = employeeWage + totalEmployeeWage
+    
 }
 
-let employeeWage = employeeHours * wagePerHour
-print(employeeWage)
+print("Total Wage For 22 Working Days is =", totalEmployeeWage)
